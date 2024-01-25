cd73b141574966a74d9bdfecda9d565f7d75dd6a
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2024-01-25 11:32:01 +0100

    Initial commit.

---
+++ src/weather__frontend/src/index.js
@@ -0,0 +1,19 @@
+import { weather__backend } from "../../declarations/weather__backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await weather__backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
